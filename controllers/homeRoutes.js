977475425096252f74f81a970c855f3526ee1d7b
Riheel Hamoande <riheel83@gmail.com>
2021-05-10 18:49:29 -0500

    Basic Login rendering

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,18 @@
 const router = require('express').Router();
 const { Secret, User } = require('../models');
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-        if (req.session.logged_in) {
-            res.redirect('/dashboard');
-            return;
-        }
+        // if (req.session.logged_in) {
+        //     res.redirect('/dashboard');
+        //     return;
+        // }
         res.render('login');
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
+
+module.exports = router;
