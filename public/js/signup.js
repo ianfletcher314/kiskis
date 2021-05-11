9995dadb022c4031aa6bce5ab21ed9168826e325
Riheel Hamoande <riheel83@gmail.com>
2021-05-10 21:52:47 -0500

    Add user signup functionality

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,35 +1,14 @@
-// const signupFormHandler = async (event) => {
-//     event.preventDefault();
-  
-//     const name = document.querySelector('#name-signup').value.trim();
-//     const email = document.querySelector('#email-signup').value.trim();
-//     const password = document.querySelector('#password-signup').value.trim();
-  
-//     if (name && email && password) {
-//       const response = await fetch('/api/users', {
-//         method: 'POST',
-//         body: JSON.stringify({ name, email, password }),
-//         headers: { 'Content-Type': 'application/json' },
-//       });
-  
-//       if (response.ok) {
-//         document.location.replace('/profile');
-//       } else {
-//         alert(response.statusText);
-//       }
-//     }
-//   };
 
-  
   const signUpFromHandler = async (event) => {
     event.preventDefault();
+    const name = document.querySelector('#user-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
-    if(email && password) {
+    
+    if(email && password && name) {
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({email, password}),
+            body: JSON.stringify({email, name, password}),
             headers: { 'Content-Type': 'application/json' },
         });
 
