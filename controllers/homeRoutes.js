15bdedaa6d2f3004c23d659ffe586c9596d2a9fc
Riheel Hamoande <riheel83@gmail.com>
2021-05-11 19:36:49 -0500

    Add user login/logout functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,25 +5,55 @@ const withAuth = require('../utils/auth');
 
 
 //homepage
-router.get('/', async (req, res) => {
-    try {
-        if (req.session.logged_in) {
-            res.redirect('/dashboard');
-            return;
-        }  
-        res.render('login');
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         if (!req.session.logged_in) {
+//             res.redirect('/login');
+//             return;
+//         }  
+//         res.render('dashboard');
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
-router.get('/login', async (req, res) => {
+// router.get('/login', async (req, res) => {
+//   // try {
+//   //     res.redirect('/');
+//   // } catch (err) {
+//   //     res.status(500).json(err);
+//   // }
+//   if(req.session.logged_in) {
+//     res.redirect('/');
+//     return
+//   }
+//   res.render('login')
+// });
+
+
+router.get('/', async (req, res) => {
   try {
-      res.redirect('/');
+      if (req.session.logged_in) {
+          res.redirect('/dashboard');
+          return;
+      }  
+      res.render('login');
   } catch (err) {
       res.status(500).json(err);
   }
 });
+
+router.get('/login', async (req, res) => {
+try {
+    res.redirect('/');
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+
+
+
 
 
 //register page will appear when click on signup
