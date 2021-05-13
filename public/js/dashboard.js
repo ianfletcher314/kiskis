c7db26ea37178b992229c4f0293d42a669850873
Brandon Maxwell <brandon.l.maxwell@gmail.com>
2021-05-12 21:35:26 -0500

    add/delete/update secret logic updated

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,46 +1,78 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#project-name').value.trim();
-  
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
-        method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create project');
-      }
+  event.preventDefault();
+
+  const title = document.querySelector('#secret-title').value.trim();
+  const body = document.querySelector('#secret-body').value.trim();
+
+  if (title && body) {
+    const response = await fetch(`/api/secrets`, {
+      method: 'POST',
+      body: JSON.stringify({ title, body }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create secret');
     }
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/projects/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete project');
-      }
+  }
+};
+
+const editButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/secrets/${id}`, {
+      method: 'PUT',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to edit secret');
     }
-  };
-  
+  }
+};
+
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/secrets/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete secret');
+    }
+  }
+};
+
+document
+  .querySelectorAll('.delete-secret')
+  .forEach((element) => {
+    element.addEventListener('click', delButtonHandler);
+  })
+
+
   document
-    .querySelector('.new-project-form')
-    .addEventListener('submit', newFormHandler);
-  
-  document
-    .querySelector('.project-list')
-    .addEventListener('click', delButtonHandler);
-  
+  .querySelectorAll('.edit-secret')
+  .forEach((element) => {
+    element.addEventListener('click', editButtonHandler);
+  })
+
+
+
+const secretForm = document
+  .querySelector('.new-secret-form')
+if (!!secretForm) {
+  secretForm.addEventListener('submit', newFormHandler)
+}
+
+
